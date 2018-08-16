4688f282c0354a0261060997297b9ef7add7e4f3
abhishek kareer <abhishekkareer92@gmail.com>
2018-08-16 15:44:42 +0530

    commiting pattren

---
+++ pattren.java
@@ -0,0 +1,16 @@
+public class pattren {
+public static void main(String [] aa)
+{
+	int i,j;
+	for(i=0;i<=4;i++)
+	{
+		j=i;
+		while(j!=0)
+		{
+			System.out.print("*");
+			j--;
+		}
+		System.out.println();
+	}
+}
+}
